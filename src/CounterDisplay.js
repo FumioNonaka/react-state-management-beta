109aba929c027cc0c7610b8a6c52e440b37f0d11
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-09-25 00:08:17 +0900

    インデントの調整

--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 	const CounterDisplay = ({ count, reset, decrement, increment }) => {
 		return (
-		<>
-		Count: {count}
-		<button onClick={reset}>Reset</button>
-		<button onClick={decrement}>-</button>
-		<button onClick={increment}>+</button>
-	</>
-	);
+			<>
+				Count: {count}
+				<button onClick={reset}>Reset</button>
+				<button onClick={decrement}>-</button>
+				<button onClick={increment}>+</button>
+			</>
+		);
 }
 
 export default CounterDisplay;
