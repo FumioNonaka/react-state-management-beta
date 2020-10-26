7fe272ef10deff8c114bac48a0e10538dd28f00f
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2020-10-27 01:19:22 +0900

    03-01 createContextとuseContextを使う

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,13 +1,13 @@
 import React, { createContext, useReducer } from 'react';
 import reducer from './reducder';
 
-const initialState = { count: 0 };
-export const AppContext = createContext(initialState);
+const initialCount = 0;
+export const AppContext = createContext(initialCount);
 export const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialCount);
 	const reset = () => dispatch({ type: 'reset' });
-	const decrement = () => dispatch((prevCount) => prevCount - 1);
-	const increment = () => dispatch((prevCount) => prevCount + 1);
+	const decrement = () => dispatch({ type: 'decrement' });
+	const increment = () => dispatch({ type: 'increment' });
 	return (
 		<AppContext.Provider value={{ count: state.count, reset, decrement, increment }}>
 			{children}
